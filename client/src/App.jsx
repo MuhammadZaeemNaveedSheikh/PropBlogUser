96933c8512131af3f1b2f206d790eb93b0f17312
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-04-28 12:51:59 +0500

    Add navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/navbar/main'
 
 function App() {
 
   return (
     <main>
+      <Navbar />
       <Outlet />
     </main>
   )
---
+++ client/src/components/navbar/config.js
@@ -0,0 +1,16 @@
+const nav_links = [
+    {
+        title: "News",
+        path: "/news",
+    },
+    {
+        title: "Forum",
+        path: "/forum",
+    },
+    {
+        title: "Contact",
+        path: "/contact",
+    }
+]
+
+export default nav_links;
